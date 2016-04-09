7e32bdad19a7daa0f683ea339757c9fcbb9e2791
Kris Kowal <kris@cixar.com>
2016-04-09 13:48:58 -0700

    Minimum viable CLI game engine

---
+++ engine.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var readline = require('readline');
+
+module.exports = ReadlineEngine;
+
+function ReadlineEngine(story, state) {
+    var self = this;
+    this.story = story;
+    this.state = state || {};
+    this.options = [];
+    this.keywords = {};
+    this.instruction = this.story.start;
+    this.readline = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    this.boundCommand = command;
+    function command(answer) {
+        self.command(answer);
+    }
+}
+
+ReadlineEngine.prototype.continue = function _continue() {
+    var _continue;
+    do {
+        if (!this['$' + this.instruction.type]) {
+            throw new Error('Unexpected instruction type');
+        }
+        _continue = this['$' + this.instruction.type](this.instruction);
+    } while (_continue);
+};
+
+ReadlineEngine.prototype.$end = function end() {
+    this.readline.close();
+    return false;
+};
+
+ReadlineEngine.prototype.$text = function text() {
+    console.log(this.instruction.text);
+    this.next();
+    return true;
+};
+
+ReadlineEngine.prototype.$option = function option() {
+    this.options.push(this.instruction);
+    for (var i = 0; i < this.instruction.keywords.length; i++) {
+        var keyword = this.instruction.keywords[i];
+        this.keywords[keyword] = this.instruction.branch;
+    }
+    this.next();
+    return true;
+};
+
+ReadlineEngine.prototype.$prompt = function prompt() {
+    this.prompt();
+    return false;
+};
+
+ReadlineEngine.prototype.$inc = function inc() {
+    if (this.state[this.instruction.name] == null) {
+        this.state[this.instruction.name] = 0;
+    }
+    this.state[this.instruction.name]++;
+    return true;
+};
+
+ReadlineEngine.prototype.$branch = function branch() {
+    if (this.state[this.instruction.name]) {
+        this.instruction = this.story[this.instruction.branch];
+    } else {
+        this.next();
+    }
+    return true;
+};
+
+ReadlineEngine.prototype.next = function next() {
+    this.instruction = this.story[this.instruction.next];
+};
+
+ReadlineEngine.prototype.command = function command(command) {
+    if (command === 'quit') {
+        this.readline.close();
+    }
+    var n = +command;
+    if (n >= 0 && n < this.options.length) {
+        this.instruction = this.story[this.options[n].branch];
+        this.flush();
+    } else if (this.keywords[command]) {
+        this.instruction = this.story[this.keywords[command]];
+        this.flush();
+    } else {
+        console.log('?');
+        this.prompt();
+    }
+};
+
+ReadlineEngine.prototype.prompt = function prompt() {
+    for (var i = 0; i < this.options.length; i++) {
+        var option = this.options[i];
+        console.log(i + '. ' + option.label);
+    }
+    this.readline.question('> ', this.boundCommand);
+};
+
+ReadlineEngine.prototype.flush = function flush() {
+    this.options.length = 0;
+    this.keywords = {};
+    this.continue();
+};
+
+function main() {
+    var story = require('./hello.json');
+    var engine = new ReadlineEngine(story);
+    engine.continue();
+}
+
+if (require.main === module) {
+    main();
+}
