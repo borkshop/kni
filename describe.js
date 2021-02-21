88463e2bfd3cf1384c0ce49287c84cbb79d023ed
Joshua T Corbin <jcorbin@wunjo.org>
2021-02-20 16:00:03 -0800

    describe: restructure around modern func syntax

--- describe.js
+++ describe.js
@@ -1,100 +1,5 @@
 'use strict';
 
-module.exports = describe;
-
-function describe(node) {
-    return types[node.type](node);
-}
-
-var types = {};
-
-types.text = function text(node) {
-    return node.text;
-};
-
-types.echo = function echo(node) {
-    return S(node.expression);
-};
-
-types.opt = function opt(node) {
-    return '(Q ' + node.question.join(' ') + ') (A ' + node.answer.join(' ') + ')';
-};
-
-types.goto = function goto(_node) {
-    return '';
-};
-
-types.call = function call(node) {
-    return node.label + '(' + node.args.map(S).join(' ') + ') esc ' + node.branch;
-};
-
-types.def = function def(node) {
-    return '(' + node.locals.join(' ') + ')';
-};
-
-types.jump = function jump(node) {
-    return node.branch + ' if ' + S(node.condition);
-};
-
-types.switch = function _switch(node) {
-    var desc = '';
-    if (node.variable) {
-        desc += '(' + node.variable + '+' +  node.value + ') ' + S(node.expression);
-    } else {
-        desc += S(node.expression);
-    }
-    desc += ' (' + node.branches.join(' ') + ') W(' + node.weights.map(S).join(' ') + ')';
-    return desc;
-};
-
-types.set = function set(node) {
-    return node.variable + ' ' + S(node.expression);
-};
-
-types.move = function move(node) {
-    return S(node.source) + ' -> ' + S(node.target);
-};
-
-types.cue = function cue(node) {
-    return node.cue;
-};
-
-types.br = function br(_node) {
-    return '';
-};
-
-types.par = function par(_node) {
-    return '';
-};
-
-types.rule = function rule(_node) {
-    return '';
-};
-
-types.startJoin = function startJoin(_node) {
-    return '';
-};
-
-types.stopJoin = function stopJoin(_node) {
-    return '';
-};
-
-types.delimit = function delimit(_node) {
-    return '';
-};
-
-types.ask = function ask(_node) {
-    return '';
-};
-
-types.read = function ask(node) {
-    var label = node.variable;
-    if (node.cue != null) {
-        label += ' ' + node.cue;
-    }
-    return label;
-};
-
 function S(args) {
     if (args[0] === 'val' || args[0] === 'get') {
         return args[1];
@@ -113,4 +18,97 @@ function V(source, target) {
     }
     r += source[i];
     return r;
+}
+
+const types = {
+    text(node) {
+        return node.text;
+    },
+
+    echo(node) {
+        return S(node.expression);
+    },
+
+    opt(node) {
+        return '(Q ' + node.question.join(' ') + ') (A ' + node.answer.join(' ') + ')';
+    },
+
+    goto(_node) {
+        return '';
+    },
+
+    call(node) {
+        return node.label + '(' + node.args.map(S).join(' ') + ') esc ' + node.branch;
+    },
+
+    def(node) {
+        return '(' + node.locals.join(' ') + ')';
+    },
+
+    jump(node) {
+        return node.branch + ' if ' + S(node.condition);
+    },
+
+    switch(node) {
+        var desc = '';
+        if (node.variable) {
+            desc += '(' + node.variable + '+' +  node.value + ') ' + S(node.expression);
+        } else {
+            desc += S(node.expression);
+        }
+        desc += ' (' + node.branches.join(' ') + ') W(' + node.weights.map(S).join(' ') + ')';
+        return desc;
+    },
+
+    set(node) {
+        return node.variable + ' ' + S(node.expression);
+    },
+
+    move(node) {
+        return S(node.source) + ' -> ' + S(node.target);
+    },
+
+    cue(node) {
+        return node.cue;
+    },
+
+    br(_node) {
+        return '';
+    },
+
+    par(_node) {
+        return '';
+    },
+
+    rule(_node) {
+        return '';
+    },
+
+    startJoin(_node) {
+        return '';
+    },
+
+    stopJoin(_node) {
+        return '';
+    },
+
+    delimit(_node) {
+        return '';
+    },
+
+    ask(_node) {
+        return '';
+    },
+
+    read(node) {
+        var label = node.variable;
+        if (node.cue != null) {
+            label += ' ' + node.cue;
+        }
+        return label;
+    },
+};
+
+module.exports = function describe(node) {
+    return types[node.type](node);
 }
