a6e3d7572198616e14fa338076bb7a6c8d9c0dec
Joshua T Corbin <jcorbin@wunjo.org>
2021-03-03 17:23:22 -0800

    story: ts-check

--- story.js
+++ story.js
@@ -1,12 +1,57 @@
+// @ts-check
+
 'use strict';
 
 var Path = require('./path');
 
+/** @typedef {import('./path').Path} PathT */
+
+/** @typedef {null|string|number} Atom */
+/** @typedef {[string, ...Atom[]]} Expr */
+/** @typedef {Atom|Expr} Arg */
+
+/** TODO structural enum on type property
+ *
+ * @typedef {object} Node
+ * @prop {string} type
+ * @prop {Arg} [next] -- NOTE only $goto isn't able to satisfy tighter string type
+ *   - is that valid?
+ *   - why can $ask lack a next?
+ *
+ * @prop {string} [lift] -- $text, $echo
+ * @prop {string} [drop] -- $text, $echo
+ * @prop {Arg} [text] -- $text
+ *
+ * @prop {Arg} [expression] -- $echo, $switch
+ * @prop {Arg} [source] -- $move
+ * @prop {Arg} [target] -- $move
+ * @prop {Arg} [locals] -- $def
+ *
+ * @prop {Arg} [variable] -- $read
+ * @prop {Arg} [cue] -- $read, $cue
+ *
+ * @prop {unknown[]} [question] -- $option
+ * @prop {unknown[]} [answer] -- $option
+ * @prop {unknown} [keywords] -- $option
+ *
+ * @prop {string} [branch] -- $call, $jump
+ * @prop {Arg} [label] -- $call
+ * @prop {Arg} [args] -- $call
+ * @prop {Arg} [condition] -- $jump
+ */
+
 module.exports = class Story {
     states = {}
 
+    /** @type {string[]} */
     errors = []
 
+    /**
+     * @param {PathT} path
+     * @param {string} type
+     * @param {Arg} arg
+     * @param {string} position
+     */
     create(path, type, arg, position) {
         const name = Path.toName(path);
         const makeNode = this.nodeMakers[type];
@@ -18,10 +63,13 @@ module.exports = class Story {
         return node;
     }
 
+    /** @param {string} error */
     error(error) {
         this.errors.push(error);
     }
 
+    /** @typedef {(arg: Arg) => Node} NodeMaker */
+    /** @type {Object<string, NodeMaker>} */
     nodeMakers = {
         text(text) { return {type: 'text', text, lift: ' ', drop: ' ', next: 'RET'} },
         echo(expression) { return {type: 'echo', expression, lift: '', drop: '', next: 'RET'} },
