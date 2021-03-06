a6be4084c08ea9eb0bf921ea8f075455e360e1a7
Joshua T Corbin <jcorbin@wunjo.org>
2021-03-06 12:26:15 -0800

    parser: ts-check
    Drop redundant check which is now handling by the type system

--- parser.js
+++ parser.js
@@ -1,18 +1,34 @@
+// @ts-check
+
 'use strict';
 
 module.exports = class Parser {
     debug = typeof process === 'object' && process.env.DEBUG_PARSER
 
+    /** @typedef {import('./scanner')} Scanner */
+    
+    /**
+     * @typedef {object} State
+     * @prop {(type: string, space: string, text: string, sc: Scanner) => State} next
+     */
+
+    /**
+     * @param {State} generator
+     */
     constructor (generator) {
         this.generator = generator;
     }
 
+    /**
+     * @param {string} type
+     * @param {string} space
+     * @param {string} text
+     * @param {Scanner} scanner
+     * @returns {Parser}
+     */
     next(type, space, text, scanner) {
         var prior = this.generator.constructor.name;
         this.generator = this.generator.next(type, space, text, scanner);
-        if (!this.generator) {
-            throw new Error(prior + ' returned undefined next state given ' + type + '/' + text + ' at ' + scanner.position());
-        }
         if (this.debug) {
             console.error(
                 'PAR',
